ad72c4a5c731f01befe1d4b99c878b152d4ec5f7
Chekesh <122931647+Chekesh@users.noreply.github.com>
2023-03-03 22:51:18 +0300

    created

---
+++ trafficlights/app/src/main/java/com/example/trafficlights/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.trafficlights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Integer colors= Color.argb(255,255,255,255);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null && savedInstanceState.containsKey("colors")){
+            colors = savedInstanceState.getInt("colors");
+            ((LinearLayout)findViewById(R.id.LL_color)).setBackgroundColor(colors);
+        }
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.containsKey("colors")){
+            colors = savedInstanceState.getInt("colors");
+            ((LinearLayout)findViewById(R.id.LL_color)).setBackgroundColor(colors);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("colors", colors);
+    }
+    public void onClikBtnAddPhone(View view){
+        colors= Color.argb(255,0,255,4);
+        LinearLayout colorView = (LinearLayout)findViewById(R.id.LL_color);
+        colorView.setBackgroundColor(colors);
+    }
+    public void onClikBtnAddPhone_2(View view){
+        colors= Color.argb(255,39,110,253);
+        LinearLayout colorView = (LinearLayout)findViewById(R.id.LL_color);
+        colorView.setBackgroundColor(colors);
+    }
+    public void onClikBtnAddPhone_3(View view){
+        colors= Color.argb(255,255,226,34);
+        LinearLayout colorView = (LinearLayout)findViewById(R.id.LL_color);
+        colorView.setBackgroundColor(colors);
+    }
+}
